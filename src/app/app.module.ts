87e551861be17df54ae85c908ebd864ad5dd59af
Narayana Rao <narayanarao4u@yahoo.com>
2020-07-11 12:15:56 +0530

    porject start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Pg1Component } from './frm/pg1/pg1.component';
+import { Pg2Component } from './frm/pg2/pg2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pg1Component,
+    Pg2Component
   ],
   imports: [
     BrowserModule,
